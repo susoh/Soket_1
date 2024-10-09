1dece648eacddf6a059a51bf08271816ef43b607
Davide Susini <susini.davide@itismeucci.com>
2024-10-09 09:16:33 +0200

    fine

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException{
+        System.out.println("Hello Word!");
+        ServerSocket ss = new ServerSocket(3645);
+        Socket mySocket = ss.accept();
+        System.out.println("qualcuno si e' collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringaRicevuta);
+
+        String stringMaiuscola = stringaRicevuta.toUpperCase();
+        out.writeBytes(stringMaiuscola + "\n");
+
+        mySocket.close();
+        ss.close();
     }
 }
